f5fedd78ee9ba1191c3d27ba868b03cafefe7e46
Milan Hommet <mhommet@teicee.com>
2024-08-01 12:47:12 +0200

    fix completion accept

--- init.lua
+++ init.lua
@@ -557,17 +557,18 @@ require('lazy').setup(
                     -- Disable "format_on_save lsp_fallback" for languages that don't
                     -- have a well standardized coding style. You can add additional
                     -- languages here or re-enable it for the disabled ones.
-                    local disable_filetypes = {
-                        c = true,
-                        cpp = true,
-                        javascript = true,
-                        typescript = true,
-                        php = true,
-                    }
-                    return {
-                        timeout_ms = 500,
-                        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-                    }
+                    -- local disable_filetypes = {
+                    --     c = true,
+                    --     cpp = true,
+                    --     javascript = true,
+                    --     typescript = true,
+                    --     php = true,
+                    -- }
+                    -- return {
+                    --     timeout_ms = 500,
+                    --     lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+                    -- }
+                    return false
                 end,
                 formatters_by_ft = {
                     lua = { 'stylua' },
@@ -642,28 +643,14 @@ require('lazy').setup(
                     --
                     -- No, but seriously. Please read `:help ins-completion`, it is really good!
                     mapping = cmp.mapping.preset.insert {
-                        -- Select the [n]ext item
-                        ['<C-n>'] = cmp.mapping.select_next_item(),
-                        -- Select the [p]revious item
-                        ['<C-p>'] = cmp.mapping.select_prev_item(),
-
                         -- Scroll the documentation window [b]ack / [f]orward
                         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
                         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-
-                        -- Accept ([y]es) the completion.
-                        --  This will auto-import if your LSP supports it.
-                        --  This will expand snippets if the LSP sent a snippet.
-                        ['<C-y>'] = cmp.mapping.confirm {
-                            select = true,
-                        },
-
                         -- If you prefer more traditional completion keymaps,
                         -- you can uncomment the following lines
-                        -- ['<CR>'] = cmp.mapping.confirm { select = true },
-                        -- ['<Tab>'] = cmp.mapping.select_next_item(),
-                        -- ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-
+                        ['<CR>'] = cmp.mapping.confirm { select = true },
+                        ['<Tab>'] = cmp.mapping.select_next_item(),
+                        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
                         -- Manually trigger a completion from nvim-cmp.
                         --  Generally you don't need this, because nvim-cmp will display
                         --  completions whenever it has completion options available.
@@ -884,6 +871,14 @@ require('lazy').setup(
                     '<leader>zm',
                     '<cmd>ZenMode<CR>',
                     desc = '[Z]en [M]ode',
+                },
+            },
+            opts = {
+                plugins = {
+                    alacritty = {
+                        enabled = true,
+                        font = '14',
+                    },
                 },
             },
         },
