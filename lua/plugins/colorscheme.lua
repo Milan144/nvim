d0da472d4da09bd67830325b7506eb9f0ced3277
Milan Hommet <mhommet@teicee.com>
2024-08-06 15:31:34 +0200

    theme dracula

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,9 @@
 return {
-    {
-        'NTBBloodbath/doom-one.nvim',
-        priority = 1000,
-        init = function()
-            vim.cmd.colorscheme 'doom-one'
-            vim.cmd.hi 'Comment gui=none'
-        end,
+  { "Mofiqul/dracula.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dracula",
     },
+  },
 }
