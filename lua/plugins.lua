0e2dfcf9b91fbdda1bd5e93437a1f0fe5101eaae
Milan Hommet <mhommet@teicee.com>
2024-08-06 11:38:26 +0200

    q

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,10 +1,13 @@
 -- PLUGIN CONFIGURATION
-require('lazy').setup({'tpope/vim-sleuth',{
-    { import = 'plugins.ai' },
-    { import = 'plugins.utility' },
-    { import = 'plugins.lsp' },
-    { import = 'plugins.git' },
-    { import = 'plugins.treesitter' },
-    { import = 'plugins.ui' },
+require('lazy').setup {
+    'tpope/vim-sleuth',
+    {
+        { import = 'plugins.ai' },
+        { import = 'plugins.utility' },
+        { import = 'plugins.lsp' },
+        { import = 'plugins.git' },
+        { import = 'plugins.treesitter' },
+        { import = 'plugins.ui' },
+        { import = 'plugins.colorscheme' },
+    },
 }
-})
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -27,30 +27,102 @@ return {{
             mode = {'n', 'v'}
         }}
     end
-}, {
+}, { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
     branch = '0.1.x',
-    dependencies = {'nvim-lua/plenary.nvim', {
+    dependencies = {'nvim-lua/plenary.nvim',
+                    { -- If encountering errors, see telescope-fzf-native README for installation instructions
         'nvim-telescope/telescope-fzf-native.nvim',
+
+        -- `build` is used to run some command when the plugin is installed/updated.
+        -- This is only run then, not every time Neovim starts up.
         build = 'make',
+
+        -- `cond` is a condition used to determine whether this plugin should be
+        -- installed and loaded.
         cond = function()
             return vim.fn.executable 'make' == 1
         end
-    }, {'nvim-telescope/telescope-ui-select.nvim'}, {
+    }, {'nvim-telescope/telescope-ui-select.nvim'}, -- Useful for getting pretty icons, but requires a Nerd Font.
+    {
         'nvim-tree/nvim-web-devicons',
         enabled = vim.g.have_nerd_font
     }},
     config = function()
-        -- Telescope configuration
-        -- (Paste the Telescope configuration code here)
-    end
-}, {
-    'NTBBloodbath/doom-one.nvim',
-    priority = 1000,
-    init = function()
-        vim.cmd.colorscheme 'doom-one'
-        vim.cmd.hi 'Comment gui=none'
+        require('telescope').setup {
+            extensions = {
+                ['ui-select'] = {require('telescope.themes').get_dropdown()}
+            }
+        }
+
+        -- Enable Telescope extensions if they are installed
+        pcall(require('telescope').load_extension, 'fzf')
+        pcall(require('telescope').load_extension, 'ui-select')
+
+        -- See `:help telescope.builtin`
+        local builtin = require 'telescope.builtin'
+        vim.keymap.set('n', '<leader>sh', builtin.help_tags, {
+            desc = '[S]earch [H]elp'
+        })
+        vim.keymap.set('n', '<leader>sk', builtin.keymaps, {
+            desc = '[S]earch [K]eymaps'
+        })
+        vim.keymap.set('n', '<leader>sf', builtin.find_files, {
+            desc = '[S]earch [F]iles'
+        })
+        vim.keymap.set('n', '<leader>ss', builtin.builtin, {
+            desc = '[S]earch [S]elect Telescope'
+        })
+        vim.keymap.set('n', '<leader>sw', builtin.grep_string, {
+            desc = '[S]earch current [W]ord'
+        })
+        vim.keymap.set('n', '<leader>sg', builtin.live_grep, {
+            desc = '[S]earch by [G]rep'
+        })
+        vim.keymap.set('n', '<leader>sd', builtin.diagnostics, {
+            desc = '[S]earch [D]iagnostics'
+        })
+        vim.keymap.set('n', '<leader>sr', builtin.resume, {
+            desc = '[S]earch [R]esume'
+        })
+        vim.keymap.set('n', '<leader>s.', builtin.oldfiles, {
+            desc = '[S]earch Recent Files ("." for repeat)'
+        })
+        vim.keymap.set('n', '<leader><leader>', builtin.buffers, {
+            desc = '[ ] Find existing buffers'
+        })
+
+        -- Slightly advanced example of overriding default behavior and theme
+        vim.keymap.set('n', '<leader>/', function()
+            -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+            builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+                winblend = 10,
+                previewer = false
+            })
+        end, {
+            desc = '[/] Fuzzily search in current buffer'
+        })
+
+        -- It's also possible to pass additional configuration options.
+        --  See `:help telescope.builtin.live_grep()` for information about particular keys
+        vim.keymap.set('n', '<leader>s/', function()
+            builtin.live_grep {
+                grep_open_files = true,
+                prompt_title = 'Live Grep in Open Files'
+            }
+        end, {
+            desc = '[S]earch [/] in Open Files'
+        })
+
+        -- Shortcut for searching your Neovim configuration files
+        vim.keymap.set('n', '<leader>sn', function()
+            builtin.find_files {
+                cwd = vim.fn.stdpath 'config'
+            }
+        end, {
+            desc = '[S]earch [N]eovim files'
+        })
     end
 }, {
     'nvim-neo-tree/neo-tree.nvim',
@@ -167,23 +239,6 @@ return {{
         -- Disable folding on alpha buffer
         vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
     end
-}, {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-        presets = {
-            bottom_search = true, -- use a classic bottom cmdline for search
-            command_palette = true, -- position the cmdline and popupmenu together
-            long_message_to_split = true, -- long messages will be sent to a split
-            inc_rename = false, -- enables an input dialog for inc-rename.nvim
-            lsp_doc_border = false -- add a border to hover docs and signature help
-        }
-    },
-    dependencies = { -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    "MunifTanjim/nui.nvim", -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
-    "rcarriga/nvim-notify"}
 }, {
     'brenoprata10/nvim-highlight-colors',
     config = function()
