dba9bec3365c07d4a0262ca233505b82aaebbe9e
Milan Hommet <mhommet@teicee.com>
2024-08-12 16:58:39 +0200

    fix theme

--- lua/milan/plugins/ui/lualine.lua
+++ lua/milan/plugins/ui/lualine.lua
@@ -2,9 +2,6 @@ return {
     'nvim-lualine/lualine.nvim',
     opts = function()
       require("lualine").setup({
-        options = {
-            theme = 'monokai-pro'
-        },
       })     
     end
 }
