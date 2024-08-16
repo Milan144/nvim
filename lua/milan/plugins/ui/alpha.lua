954dc28e5c225e96dcec481b40312449ca82d242
Milan Hommet <mhommet@teicee.com>
2024-08-16 14:48:04 +0200

    fix: dont cd $HOME when opening file explorer

--- lua/milan/plugins/ui/alpha.lua
+++ lua/milan/plugins/ui/alpha.lua
@@ -44,7 +44,7 @@ return {
         dashboard.section.buttons.val = {dashboard.button('n', '   New file', ':ene <BAR> startinsert <CR>'),
                                          dashboard.button('f', '󰮗   Find file',
             ':cd $HOME | Telescope find_files<CR>'),
-                                         dashboard.button('e', '   File Explorer', ':cd $HOME | Neotree<CR>'),
+                                         dashboard.button('e', '   File Explorer', ':Neotree<CR>'),
                                          dashboard.button('r', '   Recent', ':Telescope oldfiles<CR>'),
                                          dashboard.button('c', '   Configuration', ':e ~/.config/nvim/<CR>'),
                                          dashboard.button('R', '󱘞   Ripgrep', ':Telescope live_grep<CR>'),
