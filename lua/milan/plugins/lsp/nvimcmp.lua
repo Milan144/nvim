c04912cc91716e1c762e9d4c6282ae9d81164995
Milan Hommet <mhommet@teicee.com>
2024-08-30 09:57:57 +0200

    feat: fix autopairs

--- lua/milan/plugins/lsp/nvimcmp.lua
+++ lua/milan/plugins/lsp/nvimcmp.lua
@@ -65,5 +65,12 @@ return {
                 name = 'buffer'
             }})
         })
+
+        local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+        local cmp = require('cmp')
+        cmp.event:on(
+          'confirm_done',
+          cmp_autopairs.on_confirm_done()
+        )
     end
 }
