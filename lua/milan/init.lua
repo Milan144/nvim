f858f7b13d39b7159dcecc147ccdeed40cac4db7
Milan Hommet <mhommet@teicee.com>
2024-10-22 10:26:43 +0200

    catppuccin

--- lua/milan/init.lua
+++ lua/milan/init.lua
@@ -21,3 +21,4 @@ vim.opt.rtp:prepend(lazypath)
 -- Load plugins
 require('milan.loader')
 
+vim.cmd.colorscheme "catppuccin"
--- lua/milan/plugins/ui/colors.lua
+++ lua/milan/plugins/ui/colors.lua
@@ -1,13 +1,1 @@
-return {
-    'Mofiqul/dracula.nvim',
-    name = 'dracula',
-    config = function()
-        require('dracula').setup {
-            transparent_bg = true, -- default false  
-        }
-        -- vim.cmd 'colorscheme rose-pine'
-        -- vim.cmd("colorscheme rose-pine-main")
-        vim.cmd("colorscheme dracula")
-        -- vim.cmd("colorscheme rose-pine-dawn")
-    end,
-}
+return { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
